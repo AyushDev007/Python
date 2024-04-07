41f398b2c421fd44237fc7f542b7d2611f164fae
Ayush Kumar Singh <ayuush1018@gmail.com>
2024-04-07 23:18:01 +0530

    With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.

---
+++ que3.py
@@ -0,0 +1,8 @@
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+
+mydict={}
+n=int(input("Enter num to which upto add in dict: "))
+for i in range(1,n+1):
+    mydict[i]=i**i
+    #mydict.update({"i",i**i})
+print(mydict)
